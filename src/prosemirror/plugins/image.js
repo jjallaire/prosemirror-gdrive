5010d9c81067946a50514e762d41c7f25cf00d5e
jjallaire <jj@rstudio.com>
2019-03-28 07:04:18 -0400

    insert images w/ links rather than datauris

--- src/prosemirror/plugins/image.js
+++ src/prosemirror/plugins/image.js
@@ -21,7 +21,43 @@ export function imagePlugin(nodeType, onEditImage) {
         } else {
           return false;
         }
-      }
+      },
+
+      handleDOMEvents: {
+        drop(view, event) {
+
+          // ensure we have data transfer
+          if (!event.dataTransfer)
+            return;
+
+          // see if this is a drag of image uris
+          const uriList = event.dataTransfer.getData("text/uri-list");
+          const html = event.dataTransfer.getData("text/html");
+          if (!uriList || !html)
+            return;       
+
+          // see if we can pull an image out of the html
+          let regex = /<img.*?src=["'](.*?)["']/;  
+          let match = regex.exec(html);
+          if (!match)
+            return;
+          
+          // indicate that we can handle this drop
+          event.preventDefault();
+
+          // insert the images
+          uriList.split("\r?\n").forEach(src => {
+            // insert the image
+            const coordinates = view.posAtCoords({ 
+              left: event.clientX, 
+              top: event.clientY 
+            });
+            const node = nodeType.create({ src });
+            const transaction = view.state.tr.insert(coordinates.pos, node);
+            view.dispatch(transaction);
+          });
+        },
+      },
     },
   })
 }
