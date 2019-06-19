f93bf68c03b29f5d768388d7115122be357351cb
jjallaire <jj@rstudio.com>
2019-06-19 13:44:32 -0400

    some todo notes

--- src/prosemirror/commands/comment.js
+++ src/prosemirror/commands/comment.js
@@ -1,5 +1,15 @@
 export function commentCommand(markType) {
 
+
+  // TODO: the insertion of a comment needs to insert an additional node
+  // that is right below the paragraph containing the node. This node
+  // will be <p class="comment"> and should share an ID with this comment.
+
+  // I think we want to figure out which node we are in and then do
+  // a Transform.insert 
+
+  // Prosemirror.setBlockType
+
   return (state, dispatch) => {
 
     // must have a selection
